fb45fd8b54a4114f8737fe8c152f649217ec7435
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-05-21 10:11:56 -0700

    add UI logic

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import Triangle from './js/triangle.js';
+// import PlanetYears from './js/planetyears.js';
 
-// $(function () {
-//   $('#triangle-checker-form').on("submit", function (event) {
-//     event.preventDefault();
-//     console.log('hello');
-//     const length1 = parseInt($('#length1').val());
-//     const length2 = parseInt($('#length2').val());
-//     const length3 = parseInt($('#length3').val());
-//     const triangle = new Triangle(length1, length2, length3);
-//     const response = triangle.checkType();
-//     $('#response').append(`<p>${response}</p>`);
-//   });
+$(function () {
+  //   $('#triangle-checker-form').on("submit", function (event) {
+  //     event.preventDefault();
+  //     console.log('hello');
+  //     const length1 = parseInt($('#length1').val());
+  //     const length2 = parseInt($('#length2').val());
+  //     const length3 = parseInt($('#length3').val());
+  //     const triangle = new Triangle(length1, length2, length3);
+  //     const response = triangle.checkType();
+  //     $('#response').append(`<p>${response}</p>`);
+});
 
