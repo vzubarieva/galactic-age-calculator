cf4336ac1c4ce27eda551cfff402be27358194d2
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-05-21 22:08:19 -0700

    Add all UI logic

--- src/index.js
+++ src/index.js
@@ -2,17 +2,58 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import PlanetYears from './js/planetyears.js';
+import PlanetYears from './js/planetyears.js';
 
 $(function () {
-  //   $('#triangle-checker-form').on("submit", function (event) {
-  //     event.preventDefault();
-  //     console.log('hello');
-  //     const length1 = parseInt($('#length1').val());
-  //     const length2 = parseInt($('#length2').val());
-  //     const length3 = parseInt($('#length3').val());
-  //     const triangle = new Triangle(length1, length2, length3);
-  //     const response = triangle.checkType();
-  //     $('#response').append(`<p>${response}</p>`);
+  $('form').on("submit", function (event) {
+    event.preventDefault();
+
+    // get input values
+    const age = parseInt($('#userAge').val());
+    const lifeExpectancy = parseInt($('#lifeExpectancy').val());
+    const planetName = $("input[name=planetName]:checked").val();
+
+    // validate input values
+    if (!age || !lifeExpectancy) {
+      $("#response").html("");
+      $('#response').append(`<li class="text-danger">Please enter your age and life expectancy</li>`);
+      return;
+    }
+
+    // create instance of class
+    let planetYears = new PlanetYears(age, lifeExpectancy);
+
+    // based on planet name call methods
+    let planetAge = 0;
+    let yearsToLive = 0;
+    if (planetName === "Mercury") {
+      planetAge = planetYears.getMercuryAge();
+      yearsToLive = planetYears.getMercuryYearsToLive();
+    } else if (planetName === "Venus") {
+      planetAge = planetYears.getVenusAge();
+      yearsToLive = planetYears.getVenusYearsToLive();
+    } else if (planetName === "Mars") {
+      planetAge = planetYears.getMarsAge();
+      yearsToLive = planetYears.getMarsYearsToLive();
+    } else {
+      planetAge = planetYears.getJupiterAge();
+      yearsToLive = planetYears.getJupiterYearsToLive();
+    }
+    // display the result
+    $("#response").html("");
+    $('#response').append(`<li>Your ${planetName} age is ${planetAge}</li>`);
+    if (lifeExpectancy >= age) {
+      $("#response").append(`<li>And you will live <b>${yearsToLive}</b> more years!</li>`);
+      $("#response").append(`<div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: ${age / lifeExpectancy * 100}%" aria-valuenow="${age}" aria-valuemin="0" aria-valuemax="${lifeExpectancy}"></div>
+      </div>`);
+    } else {
+      $("#response").append(`<li>And you have lived <b>${yearsToLive}</b> years past your life expectancy!</li>`);
+      $("#response").append(`<div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: ${lifeExpectancy / age * 100}%" aria-valuenow="${age}" aria-valuemin="0" aria-valuemax="${age}"></div>
+        <div class="progress-bar bg-success" role="progressbar" style="width: ${(age - lifeExpectancy) / age * 100}%" aria-valuenow="${age - lifeExpectancy}" aria-valuemin="0" aria-valuemax="${age}"></div>
+      </div>`);
+    }
+  });
 });
 
